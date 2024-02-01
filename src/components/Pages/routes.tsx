da1da397a19538fae13a94e433731f85eb51f7bf
Mike XY <mikeuiux@gmail.com>
2024-02-01 22:26:08 +1000

    test fist commit

--- src/components/Pages/routes.tsx
+++ src/components/Pages/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./homepage";
 import Mainpage from "./mainpage";
-import ErrorPage from "./ErrorPage";
+import ErrorPage from "./errorpage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage />, errorElement: <ErrorPage /> },
