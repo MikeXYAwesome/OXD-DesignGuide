985586da6f3ad35bfdb30843eb4be94d8a83eccf
Mike XY <mikeuiux@gmail.com>
2024-02-05 13:14:27 +1000

    componets pages navigation

--- src/components/Pages/homepage.tsx
+++ src/components/Pages/homepage.tsx
@@ -1,22 +1,53 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import "./css/stylehomepage.css";
 import SplineScene from "../Splinetest";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function HomePage() {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
     <>
       <div className="homeContainer">
-        <div className="flexCard1">OXD DESIGN SYSTEM</div>
-        <div className="flexCard2">Design Phlocopy </div>
-        <div className="flexCard3">3</div>
-        <div className="flexCard4">4</div>
-        <div className="flexCard5">5</div>
+        <div className="flexCard1">
+          <div>OXD Design System</div>
+          <Link to="/mainpage">MainPage</Link>
+        </div>
+        <div className="flexCard2" data-aos="fade-up">
+          Design Philosophy
+        </div>
+        <div className="flexCard3" data-aos="fade-up">
+          OXD Interaction Module
+        </div>
+        <div className="flexCard4" data-aos="fade-up">
+          Figma UI Library
+        </div>
+        <div className="flexCard4" data-aos="fade-up">
+          User Exprince Standards
+        </div>
+        <div className="flexCard3" data-aos="fade-up">
+          Made By OXD
+        </div>
+        <div className="flexCard5" data-aos="fade-up">
+          UI Components
+        </div>
+        <div className="flexCard6" data-aos="fade-up">
+          OXD Iconography
+        </div>
+        <div className="flexCard3" data-aos="fade-up">
+          QA Guide
+        </div>
+        <div className="flexCard7" data-aos="fade-up">
+          OXD Widgets
+        </div>
       </div>
       <div className="homepagewrap">
-        <h1>HomePage</h1>
-        <div>
+        <div className="main-footer">
           <SplineScene></SplineScene>
-          <Link to="/mainpage">MainPage</Link>
         </div>
       </div>
     </>
